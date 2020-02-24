6668f0e6b0aabdcc80d820aeb083c68c023b73a9
mzelensky <max.m.zelensky@gmail.com>
2020-02-24 16:21:34 +0100

    Initial commit

--- Algorithms/bubble sort/bs.js
+++ Algorithms/bubble sort/bs.js
@@ -1,4 +1,20 @@
-﻿function bubbleSort(arr){
+﻿//speed test
+
+console.time('bubbleSort');
+bubbleSort(myArr);
+console.timeEnd('bubbleSort');
+console.log(bubbleSort(myArr));
+
+var t0 = performance.now();
+bubbleSort(myArr);
+var t1 = performance.now();
+console.log(bubbleSort(myArr));
+console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
+
+//-------------
+
+function bubbleSort(arr){
+	
 	var arr_len = arr.length;
 	for (var i = 0; i < arr_len ; i++) {
         for(var j = 0 ; j < arr_len - i - 1; j++){
